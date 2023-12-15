720251d4da1aa3a3243a225a5605173f38d72c85
BT_1N3_12 <sample@bths.edu>
2023-12-15 09:14:43 -0500

    merge conflict

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(":)");
         System.out.println(">:)");
-        System.out.println("Hey gangster");
+        System.out.println("Hey gangsters");
         System.out.println("Doofust");
         System.out.println(":(:(:(");
     }
